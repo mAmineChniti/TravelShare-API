0042891fff5167e5feb736a41d33a594b225e148
mAmineChniti <emin.chniti@esprit.tn>
2025-02-07 13:58:54 +0100

    init repo

---
+++ src/main/java/tn/esprit/utils/dbCon.java
@@ -0,0 +1,33 @@
+package tn.esprit.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class dbCon {
+    private static dbCon instance;
+    private static final String URL = "jdbc:mysql://localhost:3306/TravelShare";
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASS = System.getenv("DB_PASS");
+    private Connection con;
+
+    private dbCon() {
+        try {
+            con = DriverManager.getConnection(URL, USER, PASS);
+            System.out.println("we hacked the mainframe");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static dbCon getInstance() {
+        if (instance == null) {
+            instance = new dbCon();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return con;
+    }
+}
