c708d3ee9e7f056ce54971c8702b12ad536c7211
mAmineChniti <emin.chniti@esprit.tn>
2025-02-08 12:08:50 +0100

    added IService interface, and .env file example

---
+++ src/main/java/tn/esprit/services/IService.java
@@ -0,0 +1,11 @@
+package tn.esprit.services;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IService <T>{
+    public void add(T t) throws SQLException;
+    public void update(T t) throws SQLException;
+    public void delete(int id) throws SQLException;
+    public List<T> ListAll() throws SQLException;
+}
