d2df461ef89ca2868a750e5f0a998effcbfff71a
Wafacg <wafachabbi21@gmail.com>
2025-02-10 14:40:29 +0100

    j'ai ajouté entité guide

---
+++ src/main/java/tn/esprit/entities/Guide.java
@@ -0,0 +1,39 @@
+package tn.esprit.entities;
+
+public class Guide {
+    private int id_guide;
+    private String name , lastname;
+    private String description;
+
+    public int getId_guide() {
+        return id_guide;
+    }
+
+    public void setId_guide(int id_guide) {
+        this.id_guide = id_guide;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
