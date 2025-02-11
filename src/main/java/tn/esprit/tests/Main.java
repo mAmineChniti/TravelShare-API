4745af0aed255dc0a999361f5dd73e70fad7f044
mAmineChniti <emin.chniti@esprit.tn>
2025-02-11 12:10:34 +0100

    fixed env variables

--- src/main/java/tn/esprit/tests/Main.java
+++ src/main/java/tn/esprit/tests/Main.java
@@ -1,7 +1,10 @@
 package tn.esprit.tests;
 
+import tn.esprit.utils.dbCon;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(dbCon.getInstance().getConnection());
     }
 }
