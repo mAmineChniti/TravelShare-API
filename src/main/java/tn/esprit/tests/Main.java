3f00f639fc82a573b2be8b455f0a5cea9b22e85d
adem sahbani <adem.sahbani@esprit.tn>
2025-02-12 19:51:25 +0100

    Finishes Services for OffreVoyages and ReservationPack

--- src/main/java/tn/esprit/tests/Main.java
+++ src/main/java/tn/esprit/tests/Main.java
@@ -1,7 +1,9 @@
 package tn.esprit.tests;
 
+import tn.esprit.utils.dbCon;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println(dbCon.getInstance().getConnection());
     }
 }
