60be291608e80493f81731098b0b253202a9f12d
mAmineChniti <emin.chniti@esprit.tn>
2025-02-27 22:17:43 +0100

    added comments section

--- src/main/java/tn/esprit/services/CommentsService.java
+++ src/main/java/tn/esprit/services/CommentsService.java
@@ -1,14 +1,14 @@
 package tn.esprit.services;
 
-import tn.esprit.utils.dbCon;
 import tn.esprit.entities.Comments;
+import tn.esprit.utils.dbCon;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CommentsService implements IService<Comments> {
-    private Connection connection;
+    private final Connection connection;
 
     public CommentsService() {
         connection = dbCon.getInstance().getConnection();
@@ -64,6 +64,32 @@ public class CommentsService implements IService<Comments> {
                 comment.setComment(rs.getString("comment"));
                 comment.setCommented_at(rs.getDate("commented_at"));
                 comment.setUpdated_at(rs.getDate("updated_at"));
+                comments.add(comment);
+            }
+        }
+        return comments;
+    }
+
+    public List<Comments> fetchById(int post_id) throws SQLException {
+        List<Comments> comments = new ArrayList<>();
+        String req = "SELECT c.comment_id, c.post_id, c.commenter_id, c.comment, c.commented_at, c.updated_at, u.name, u.last_name "
+                + "FROM comments c "
+                + "JOIN users u ON c.commenter_id = u.user_id "
+                + "WHERE c.post_id = ? "
+                + "ORDER BY c.commented_at DESC";
+        try (PreparedStatement ps = connection.prepareStatement(req)) {
+            ps.setInt(1, post_id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Comments comment = new Comments();
+                comment.setComment_id(rs.getInt("comment_id"));
+                comment.setPost_id(rs.getInt("post_id"));
+                comment.setCommenter_id(rs.getInt("commenter_id"));
+                comment.setComment(rs.getString("comment"));
+                comment.setCommented_at(rs.getDate("commented_at"));
+                comment.setUpdated_at(rs.getDate("updated_at"));
+                comment.setCommenter_name(rs.getString("name"));
+                comment.setCommenter_lastname(rs.getString("last_name"));
                 comments.add(comment);
             }
         }
